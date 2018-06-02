0992717125c828fe175def0638b664b05bb647e9
Kevin <cushing.kev@gmail.com>
2018-06-01 20:06:01 -0700

    lets break stuff

--- 5051/Controllers/RemoteStudentController.cs
+++ 5051/Controllers/RemoteStudentController.cs
@@ -3,28 +3,104 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using _5051.Models;
+using _5051.Backend;
 
 namespace _5051.Controllers
 {
     public class RemoteStudentController : Controller
     {
+        // A ViewModel used for the Student that contains the StudentList
+        private StudentViewModel StudentViewModel = new StudentViewModel();
+
+        // The Backend Data source
+        private StudentBackend StudentBackend = StudentBackend.Instance;
         // GET: /AdminPanel/Options/someName
-        public ActionResult Report(String username = null)
+        public ActionResult Report()
         {
-            if (username == null) username = "Student"; 
-            ViewBag.Message = "Welcome " + username + "!";
-            return View();
+            // Query backend to refresh every time Index() is called
+            var myDataList = StudentBackend.Index();
+            var StudentViewModel = new StudentViewModel(myDataList);
+            var StudentModel = new StudentModel(StudentViewModel.StudentList[0]);
+
+            return View(StudentModel);
         }
         //Return Achievements page
         public ActionResult Achievements()
         {
             return View();
         }
+        public string[] avatar_names = new string[]
+        {
+            "person1.png", "person2.png", "person3.png",
+            "person4.png", "person5.png", "person6.png"
+        };
+        
         //Returns Avatar select page
         public ActionResult ChooseAvatar()
         {
-            return View();
+            
+            // Query backend to refresh every time Index() is called
+            var myDataList = StudentBackend.Index();
+            var StudentViewModel = new StudentViewModel(myDataList);
+            var StudentModel = new StudentModel(StudentViewModel.StudentList[0]);
+            ViewBag.avatar_names = avatar_names;
+            
+            return View(StudentModel);
         }
+
+
+         // NOTE: Scott, thanks for the code
+        /// <summary>
+        /// Choose avatar and update the student model
+        /// </summary>
+        /// <param name="data"></param>
+        /// <returns></returns>
+        // POST: Student/ChooseAvatar
+        [HttpPost]
+        public ActionResult ChooseAvatar([Bind(Include=
+                                        "Id,"+
+                                        "Name,"+
+                                        "Username,"+
+                                        "AvatarId,"+
+                                        "IsActive,"+
+                                        "IsEdit,"+
+                                        "LoginStatus,"+
+                                        "DailyStatus,"+
+                                        "TimeIn,"+
+                                        "TimeOut,"+
+                                        "Password,"+
+                                        "")] StudentModel data)
+        {
+            if (!ModelState.IsValid)
+            {
+                // Send back for edit
+                return View(data);
+            }
+
+            if (data == null)
+            {
+                // Send to Error Page
+                return RedirectToAction("Error", new { route = "Home", action = "Error" });
+            }
+
+            if (string.IsNullOrEmpty(data.Id))
+            {
+                // Return back for Edit
+                return View(data);
+            }
+
+
+            // Make it official
+            StudentBackend.Update(data);
+
+            return RedirectToAction("Index");
+        }
+
+        //public ActionResult ChooseAvatar()
+       // {
+         //   
+        //}
         //Returns student history page
         public ActionResult StudentHistory()
         {
